9b3ebbb29028c83e70bd37beaf6616ea2f4673ae
createlineageos1 <axfootballcards@gmail.com>
2025-04-28 18:18:07 +0200

    Update commands.py

--- commands.py
+++ commands.py
@@ -1,65 +1,113 @@
 import random
 from termcolor import colored
 import os
-import time
-from kernel import process_management
 import webbrowser
+from kernel import process_management
 
 usr = "hexrail"
-os = "HEXRAIL 1.1 EMULATE"
-ui = "cli"
+os_version = "hexrail 1.3 KERNEL_UPDATE"
+ui = "CLI_based"
 
 class CommandProcessor:
-  @staticmethod
-  @process_management(priority=3)
-  def cmd():
-    print("showfetch, usrdata, systurnoff, bored, ota, hme")
+    @staticmethod
+    @process_management(priority=1)
+    def ota():
+        print("Opening OTA link...")
+        webbrowser.open("https://www.figma.com/proto/Xl70GkHcPBv0fywmeZT2FT/hexrail-updates?node-id=1-2&p=f&t=3DvVl8pyKf0qqzeQ-0&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1")
 
-  @staticmethod
-  @process_management(priority=1)
-  def ota():
-    webbrowser.open("https://www.figma.com/proto/gVbll3jSnVPksZsyF6wghE/Untitled?node-id=0-1&p=f&t=4wQynKV5aFOD5vNf-0&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1")
+    @staticmethod
+    @process_management(priority=2)
+    def usrdata():
+        print(usr)
 
-  @staticmethod
-  @process_management(priority=2)
-  def usrdata():
-    print(usr)
+    @staticmethod
+    @process_management(priority=1)
+    def systurnoff():
+        confirm = input("Are you sure you want to turn off the system? (yes/no): ")
+        if confirm.lower() == "yes":
+            print("System turning off...")
+            exit()
+        else:
+            print("Cancelled.")
 
-  @staticmethod
-  @process_management(priority=1)
-  def systurnoff():
-    exit()
+    @staticmethod
+    @process_management(priority=3)
+    def showfetch():
+        print("User:", usr)
+        print("OS:", os_version)
+        print("UI:", ui)
 
-  @staticmethod
-  @process_management(priority=3)
-  def showfetch():
-    print("User:", usr)
-    print("OS:", os)
-    print("UI:", ui)
+    @staticmethod
+    @process_management(priority=1)
+    def hme():
+        print("Opening HexMobEnv...")
+        webbrowser.open("https://www.figma.com/proto/6BIq0bwjp4UNTA4k137S5L/hexmobenv?node-id=1-1587&t=VtsHSvo6nCksWXqK-0&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1")
 
-  @staticmethod
-  @process_management(priority=1)
-  def hme():
-      webbrowser.open("https://www.figma.com/proto/6BIq0bwjp4UNTA4k137S5L/hexmobenv?node-id=1-1587&t=VtsHSvo6nCksWXqK-0&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1")
+    @staticmethod
+    @process_management(priority=1)
+    def bored():
+        bored_commands = [
+            "play a game",
+            "watch a movie",
+            "read a book",
+            "go for a walk",
+            "listen to music",
+            "try a new recipe",
+            "learn something new",
+            "call a friend",
+            "do some exercise",
+            "meditate"
+        ]
+        print("You can:", random.choice(bored_commands))
 
-  @staticmethod
-  @process_management(priority=2)
-  def bored():
-    print("AI:", random.choice([
-        "You can play a game.",
-        "If youre bored, watch some movies. Some good ones are: The Matrix, Inception, Interstellar, Cassandra, Rick and Morty, and The Simpsons.",
-        "If youre bored, you can go outside and take a walk. Fresh air is good for you.",
-        "If youre bored, read a book. Some good ones are: The Hitchhiker's Guide to the Galaxy, 1984, Brave New World, and The Great Gatsby.",
-        "Listen some musics, podcasts.",
-        "If youre a good programmer, you can try to make a game, or anything you want.",
-        "Read Wikipedia articles, to learn something new!",
-        "Yoou can cook something new, or try to make a new recipe.",
-        "You can watch social media, like TikTok, Instagram, or Twitter.",
-        "You can play with your pets, if you have any.",
-    ]))
+    @staticmethod
+    @process_management(priority=1)
+    def watch():
+        print("Opening YouTube...")
+        webbrowser.open("https://www.youtube.com/")
 
-  @staticmethod
-  @process_management(priority=1)
-  def trigger_panic():
-    time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-    print("Current date and time:", time)
+    @staticmethod
+    @process_management(priority=1)
+    def movie():
+        print("Opening Netflix...")
+        webbrowser.open("https://www.netflix.com/")
+
+    @staticmethod
+    @process_management(priority=1)
+    def cmd():
+        """Basic command processor"""
+        print(colored("Welcome to HexrailTV CLI.", "green"))
+        print(colored("Available commands:", "cyan"))
+        print(colored("showfetch - Display system information", "yellow"))
+        print(colored("usrdata - Display user data", "yellow"))
+        print(colored("systurnoff - Turn off the system", "yellow"))
+        print(colored("ota - Open OTA update page", "yellow"))
+        print(colored("hme - Open HexMobEnv page", "yellow"))
+        print(colored("bored - Suggest an activity when bored", "yellow"))
+        print(colored("watch - Open YouTube", "yellow"))
+        print(colored("movie - Open Netflix", "yellow"))
+        print(colored("exit - Exit the system", "yellow"))
+        
+        while True:
+            command = input(colored(f"{usr}@{os_version} > ", "yellow"))
+            if command == "showfetch":
+                CommandProcessor.showfetch()
+            elif command == "usrdata":
+                CommandProcessor.usrdata()
+            elif command == "systurnoff":
+                CommandProcessor.systurnoff()
+            elif command == "ota":
+                CommandProcessor.ota()
+            elif command == "hme":
+                CommandProcessor.hme()
+            elif command == "bored":
+                CommandProcessor.bored()
+            elif command == "watch":
+                CommandProcessor.watch()
+            elif command == "movie":
+                CommandProcessor.movie()
+            elif command == "exit":
+                print("Exiting HexrailTV...")
+                break
+            else:
+                print(colored(f"Unknown command: {command}", "red"))
