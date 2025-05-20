1e251818f790b88776cc7ed1f4be02a48f990b50
createlineageos1 <axfootballcards@gmail.com>
2025-05-20 17:32:45 +0200

    Create installer_debian.sh

---
+++ installer_debian.sh
@@ -0,0 +1,8 @@
+echo "MAKE SURE THAT YOU ARE IN HEXRAIL DIRECTORY!"
+sleep 3
+echo "Hexrail 1.2"
+sleep 3
+echo "Installing python3 language..."
+sudo apt install python3
+cd hexrail/
+python3 main.py
