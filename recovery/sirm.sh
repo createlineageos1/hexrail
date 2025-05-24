f78ea733dff2b55452f42fdf9cd83f71a2d7a55c
isaac <axfootballcards@gmail.com>
2025-05-24 15:07:05 +0200

    Create sirm.sh

---
+++ recovery/sirm.sh
@@ -0,0 +1,10 @@
+echo [+] SIRM Stock Image Recovery Mode
+echo Make sure that git and python3 is installed
+sleep 2
+echo [+] Flashing new system... DO NOT PRESS CTRL + C WHILE INSTALLING THE HEXRAIL SYSTEM! THIS MAY ABORT THE INSTALLATION COMPLETELY!
+git clone https://github.com/createlineageos1/hexrail
+cd hexrail
+echo The main Hexrail directory located at:
+pwd
+echo [+] Installation finished, booting to Hexrail system...
+python3 main.py
