d56ef4c50eb119479fa21c60cdd0aa88b4929944
isaac <axfootballcards@gmail.com>
2025-05-31 17:00:32 +0200

    Update sirm.sh

--- recovery/sirm.sh
+++ recovery/sirm.sh
@@ -1,5 +1,6 @@
 echo [+] SIRM Stock Image Recovery Mode
 echo Make sure that git, python3 and termcolor is installed
+echo WARNING! ENTERING SIRM MODE MAY DOWNGRADE YOUR VERSION, IF THE HEXROID SYSTEM IS CORRUTED, DOWNLOAD THE SYSTEM ON OFFICIAL GITHUB PAGE, PRESS CTRL+C TO CANCEL THIS OPERATION
 sleep 2
 echo [+] Flashing new system... DO NOT PRESS CTRL + C WHILE INSTALLING THE HEXRAIL SYSTEM! THIS MAY ABORT THE INSTALLATION COMPLETELY!
 git clone https://github.com/createlineageos1/hexrail
