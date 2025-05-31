013e8e181c57b970f969f0d69f65d803115daa79
isaac <axfootballcards@gmail.com>
2025-05-31 23:30:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,51 @@
 import os 
 import sys
 import time
-import commands
 from datetime import date
 from termcolor import colored
 from commands import CommandProcessor
 from kernel import Kernel
+from package_manager import ensure_dirs, list_repo_packages, list_installed_packages, install_package, run_package, help_menu
+import webbrowser
+import subprocess
 
+uiboot = input("Do you want to boot into Hexroid UI? (y/n): ").strip().lower()
+
+if uiboot == 'y':
+    webbrowser.open("https://www.figma.com/proto/6BIq0bwjp4UNTA4k137S5L/hexmobenv?node-id=1-417&t=JoCsnjV4sPCr4Qxk-0&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1")
+
+else:
+    print("Booting...")
+
+password = input("Set a password for Hexroid: ")
+
+os.system("cls" if os.name == "nt" else "clear")
+
+password_confirm = input("Enter password: ")
 
 cp = CommandProcessor()
 
 ascii = r"""
-  _                           _ 
- | |                       (_) |
- | |__   _____  ___ __ __ _ _| |
- | '_ \ / _ \ \/ / '__/ _` | | |
- | | | |  __/>  <| | | (_| | | |
- |_| |_|\___/_/\_\_|  \__,_|_|_|
-                   
+
+ /$$   /$$                                         /$$       /$$
+| $$  | $$                                        |__/      | $$
+| $$  | $$  /$$$$$$  /$$   /$$  /$$$$$$   /$$$$$$  /$$  /$$$$$$$
+| $$$$$$$$ /$$__  $$|  $$ /$$/ /$$__  $$ /$$__  $$| $$ /$$__  $$
+| $$__  $$| $$$$$$$$ \  $$$$/ | $$  \__/| $$  \ $$| $$| $$  | $$
+| $$  | $$| $$_____/  >$$  $$ | $$      | $$  | $$| $$| $$  | $$
+| $$  | $$|  $$$$$$$ /$$/\  $$| $$      |  $$$$$$/| $$|  $$$$$$$
+|__/  |__/ \_______/|__/  \__/|__/       \______/ |__/ \_______/
+                                                                
+                                                                
 """
+
+if password == password_confirm:
+    print("Password set successfully.")
+
+else:
+    print("Password doesn't match.")
+    sys.exit("Exiting...")
+
 
 def loading():
     for i in range(0, 101):
@@ -38,22 +65,18 @@ system_commands = [
     'cp.usrdata',
     'cp.hme',
     'cp.watch',
-    'cp.movie'
+    'cp.movie',
 ]
 
 kernel = Kernel()
 
 loading()
 
-c = colored
-print(c("hexrail under APACHE-2.0 LICENSE.", "yellow"))
-
 today = date.today()
 print("Today's date:", today)
 c = colored
-print(c("security patch: 1 march 2024", "red"))
 while True:
-    user_input = input(f"usr@hexrail:{os.curdir}/$>> ").split(" ")
+    user_input = input(f"Enter command or app name 'cmd' for help:").split(" ")
     entered_command = user_input[0]
     args = user_input[1:]
     
